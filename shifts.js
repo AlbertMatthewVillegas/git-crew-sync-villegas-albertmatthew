883984c932968fae45113b0ae6cad441219b39d6
Albert Matthew Villegas <12mnsin@gmail.com>
2026-09-22 00:50:40 +0800

    Round shift pay instead of truncating

--- shifts.js
+++ shifts.js
@@ -2,8 +2,9 @@ function isValidShift(hours) {
   return hours > 0 && hours <= 24;
 }
 
-function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+function calculatePay(hoursWorked, hourlyRate) {
+  const pay = hoursWorked * hourlyRate;
+  return Math.round(pay);
 }
 
 module.exports = { isValidShift, calculatePay };
