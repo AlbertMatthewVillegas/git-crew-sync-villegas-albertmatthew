770feba57fb40b6d9afd5d2fcc1a943aea73c6d6
Albert Matthew Villegas <12mnsin@gmail.com>
2026-09-22 01:13:23 +0800

    merge commit

--- shifts.js
+++ shifts.js
@@ -3,8 +3,19 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hoursWorked, hourlyRate) {
-  const pay = hoursWorked * hourlyRate;
+  if (hoursWorked < 0 || hourlyRate < 0) {
+    throw new Error("Hours and rate must be non-negative");
+  }
+  let pay;
+  if (hoursWorked > 8) {
+    const regular = 8 * hourlyRate;
+    const overtime = (hoursWorked - 8) * hourlyRate * 1.5;
+    pay = regular + overtime;
+  } else {
+    pay = hoursWorked * hourlyRate;
+  }
   return Math.round(pay);
 }
+
 
 module.exports = { isValidShift, calculatePay };
